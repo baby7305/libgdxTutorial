d9f916dfe359b98fd6000d0841e99fc511da11c7
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-03 15:31:30 +0800

    雨滴与水桶碰撞消失，有水声

--- core/src/com/badlogic/drop/Drop.java
+++ core/src/com/badlogic/drop/Drop.java
@@ -89,6 +89,10 @@ public class Drop extends ApplicationAdapter {
 			Rectangle raindrop = iter.next();
 			raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
 			if(raindrop.y + 64 < 0) iter.remove();
+			if(raindrop.overlaps(bucket)) {
+				dropSound.play();
+				iter.remove();
+			}
 		}
 
 
